563f2a408fd49ee181c53c73e4cec6e16bc1daad
hennyvalsrestaurant-rgb <hennyvalsrestaurant@gmail.com>
2026-06-01 05:19:12 -0400

    fix: add missing closing brace to HOME page

--- src/pages/HOME.tuckg.js
+++ src/pages/HOME.tuckg.js
@@ -1,7 +1,6 @@
 // Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 
 $w.onReady(function () {
-
 	// Write your Javascript code here using the Velo framework API
 
 	// Print hello world:
@@ -11,3 +10,4 @@ $w.onReady(function () {
 	// $w("#button1").label = "Click me!";
 
 	// Click "Run", or Preview your site, to execute your code
+});
